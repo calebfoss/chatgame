d3aa99676811247f6e399ef41119094ce887f8da
Caleb Foss <calebfoss@gmail.com>
2018-03-30 19:20:05 -0500

    Added openshift configuration

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-
+// Check the configuration file for more details
+var config = require('./config');
 var express = require('express');
-var app = express();
-var server = app.listen(8080);
+var app = require('express')();
+var server = require('http').Server(app);
+// var app = express();
+// var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || '8080');
 var clientIndex = 0;
 var clients = [];
 
-app.use(express.static('public'));
+// var io = socket(server);
+var io = require('socket.io')(server);
 
-console.log("My socket server is running");
+console.log("Trying to start server with config:", config.serverip + ":" + config.serverport);
 
+// Both port and ip are needed for the OpenShift, otherwise it tries 
+// to bind server on IP 0.0.0.0 (or something) and fails
+server.listen(config.serverport, config.serverip, function() {
+  console.log("Server running @ http://" + config.serverip + ":" + config.serverport);
+});
+
+app.use(express.static(__dirname + '/'));
+// Server GET on http://domain/api/config
+// A hack to provide client the system config
+console.log(JSON.stringify(config));
+app.get('/api/config', function(req, res) {
+  res.send('var config = ' + JSON.stringify(config));
+});
+// app.use(express.static('public'));
 var socket = require('socket.io');
 
-var io = socket(server);
+
 
 io.sockets.on('connection', newConnection);
 
