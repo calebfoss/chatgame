ffcee6a64583df400939905b99486533aa513be4
Caleb Foss <calebfoss@gmail.com>
2018-03-31 20:33:26 -0500

    Game prototype

--- public/IMgame.js
+++ public/IMgame.js
@@ -1,30 +1,166 @@
 var socket;
-var chat;
-var input;
-// var connString = config.protocol + config.domain + ':' + config.clientport;
-// console.log("Websocket connection string:", connString, config.wsclientopts);
-// socket = io.connect('http://localhost:8080');
 socket = io.connect();
-// socket = io.connect('http://chatgame-chatgame.a3c1.starter-us-west-1.openshiftapps.com:8080');
-socket.on('input', newInput);
-chat = document.getElementById("chat");
-input = document.getElementById("input");
-input.focus();
-input.select();
 
+var main = document.getElementById('main');
+var codeName;
+var realName;
+var currentConvo;
+var convoTime = 180;
 
-function newInput(log){
-	chat.innerHTML += log.id + ": " + log.value + "<br>";
-	chat.scrollTop = chat.scrollHeight;
+startScreen();
+
+function startScreen(){
+	//Create elements for start
+	var instructions = document.createElement('p');
+	instructions.id = 'instructions';
+	instructions.innerHTML = "Welcome to CHATGAME&trade;! Please type your real first name and press enter.";
+	main.appendChild(instructions);
+	var nameInput = document.createElement('input');
+	nameInput.id = 'nameInput';
+	nameInput.type = 'text';
+	main.appendChild(nameInput);
+	nameInput.focus();
+	nameInput.select();
+
+	socket.on('codeName', getCodeName);
+
+	//Check for name submission
+	nameInput.addEventListener('keyup',function(event){
+		if (event.keyCode === 13) {
+			var data = {
+				value : nameInput.value
+			}
+			realName = nameInput.value;
+			socket.emit('realName',data);
+			nameInput.value = "";
+		}
+	});
+
+	function getCodeName(CodeName){
+		codeName = CodeName;
+		main.removeChild(nameInput);
+		instructionScreen();
+	}
+
 }
 
-input.addEventListener('keyup',function(event){
-	if (event.keyCode === 13) {
-		var data = {
-			value : input.value
+function instructionScreen(){
+	var instructions = document.getElementById('instructions');
+	var message = "Hello, "+realName+". Your new code name is "+codeName+
+	". The other players each have a code name as well. Your objective "+
+	"is to discover the real identities of each of the other players, while "+
+	"keeping your own identity a secret. You will do this by "+
+	"having a conversation with them via instant message. You will be scored "+
+	"based on this system:<br><br>1 point for each other player you guess correctly<br>"+
+	"1 point for each player who does not guess your identity<br>"+
+	"-1 point for being the least talkative player<br><br>"+
+	"Each player chooses a conversation topic. You will discuss the topic for 3 minutes.<br>"+
+	"Enter in your topic:";
+	;
+	instructions.innerHTML = message;
+	var convoInput = document.createElement('input');
+	convoInput.id = 'nameInput';
+	convoInput.type = 'text';
+	main.appendChild(convoInput);
+	convoInput.focus();
+	convoInput.select();
+
+	convoInput.addEventListener('keyup',function(event){
+		if (event.keyCode === 13) {
+			var data = {
+				value : convoInput.value
+			}
+			socket.emit('convo', data);
+			main.removeChild(convoInput);
+			waitingForPlayers();
 		}
-		console.log(data);
-		socket.emit('input',data);
-		input.value = "";
+	});
+}
+
+function waitingForPlayers(){
+	var instructions = document.getElementById('instructions');
+	instructions.innerHTML = "These are the players so far:<br>";
+	var players = document.createElement('h2');
+	main.appendChild(players);
+	var playerList = [""];
+	var buttonInst = document.createElement('p');
+	buttonInst.id = 'buttonInst';
+	buttonInst.innerHTML = "When all players are listed, press the play button."
+	main.appendChild(buttonInst);
+	var playButton = document.createElement('button');
+	playButton.innerHTML = "PLAY";
+	socket.on('play', function(convo){
+		currentConvo = convo;
+		main.removeChild(players);
+		main.removeChild(buttonInst);
+		main.removeChild(playButton);
+		chat();
+	});
+	playButton.onclick = function(){
+		socket.emit('play');
+	};
+	main.appendChild(playButton);
+
+	socket.on('players', addToPlayerList);
+
+	function addToPlayerList(PlayerList){
+		for(var i = 0; i < PlayerList.length; i++){
+			if(playerList.indexOf(PlayerList[i])==-1){
+				playerList.push(PlayerList[i]);
+				players.innerHTML += PlayerList[i];
+				players.innerHTML += '<br>'
+			}
+		}
 	}
-});
+
+}
+
+function chat(){
+	//Create elements for chat
+	var instructions = document.getElementById('instructions');
+	instructions.innerHTML = "Talk about: "+currentConvo;
+	var chatLog = document.createElement('div');
+	chatLog.id = 'chat';
+	main.appendChild(chatLog);
+	var inputDiv = document.createElement('div');
+	inputDiv.id = 'inputDiv';
+	main.appendChild(inputDiv);
+	var input = document.createElement('input');
+	input.id = 'input';
+	input.type = 'text';
+	inputDiv.appendChild(input);
+	input.focus();
+	input.select();
+
+	//Check for new chat logs
+	socket.on('input', newInput);
+	socket.on('newConvo', function(data){
+		currentConvo = data;
+		instructions.innerHTML = "Talk about "+currentConvo+" for "+convoTime+" seconds";
+	});
+	socket.on('time', function(data){
+		convoTime = data;
+		instructions.innerHTML = "Talk about "+currentConvo+" for "+convoTime+" seconds";
+
+	})
+	socket.on('endChat', function(){
+		instructions.innerHTML = "Game over";
+	});
+
+	//Submit text	
+	input.addEventListener('keyup',function(event){
+		if (event.keyCode === 13) {
+			var data = {
+				value : input.value
+			}
+			socket.emit('input',data);
+			input.value = "";
+		}
+	});
+
+	function newInput(log){
+		chatLog.innerHTML += log.id + ": " + log.value + "<br>";
+		chatLog.scrollTop = chatLog.scrollHeight;
+	}
+}
+
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var clientIndex = 0;
 var clients = [];
-// Based off of Shawn Van Every's Live Web
-// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+var PlayerList = [];
+var ConvoList = [];
+var convoTime;
+var convoIndex;
 
-// Using express: http://expressjs.com/
 var express = require('express');
 // Create the app
 var app = express();
 
-// Set up the server
-// process.env.PORT is related to deploying on heroku
 var server = app.listen(8080, listen);
 
 // This call back just tells us that the server has started
@@ -29,27 +28,80 @@ io.sockets.on('connection', newConnection);
 
 
 function newConnection(socket){
-	var index = clientIndex;
+	var codeName = Math.floor(Math.random()*100);
+	var realName;
+	var convo;
 
 	clients.push({"id":socket.id,"index":clientIndex});
 	clientIndex ++;
 	console.log('New connection: '+socket.id+" Client count: "+io.engine.clientsCount);
 	socket.on('input',input);
+	socket.on('realName',getRealName);
+	socket.on('convo', getConvo);
+	socket.on('play', startPlay);
 	function input(data){
 		var log  = {
-			id : index,
+			id : codeName,
 			value : data.value
 		}
 		io.sockets.emit('input',log);
+	}
+	function getRealName(data){
+		realName = data.value;
+		console.log(realName);
+		socket.emit('codeName', codeName);
+	}
+	function getConvo(data){
+		convo = data.value;
+		ConvoList.push(convo);
+		PlayerList.push(realName);
+		io.sockets.emit('players', PlayerList);
+	}
+	function startPlay(){
+		shuffle(ConvoList);
+		io.sockets.emit('play', ConvoList[0]);
+		convoTime = 180;
+		sendTime();
+	}
+	function sendTime(){
+		io.sockets.emit('time',convoTime);
+		convoTime -= 1;
+		if(convoTime > 0){
+			setTimeout(function(){sendTime();}, 1000);
+		}
+		else{
+			convoIndex++;
+			if(convoIndex < ConvoList.length){
+				io.sockets.emit('newConvo', ConvoList[convoIndex]);
+				convoTime = 180;
+				setTimeout(sendTime, 1000);
+			}
+			else{
+				io.sockets.emit('endChat');
+			}
+		}
+		
 	}
 	socket.on('disconnect', function(){
 		console.log("lost connection");
 		var log = {
 			id : "ALERT",
-			value : index+" has left"
+			value : codeName+" has left"
 		}
 		io.sockets.emit('input',log);
+		ConvoList.splice(PlayerList.indexOf(realName),1);
+		PlayerList.splice(PlayerList.indexOf(realName),1);
 	});
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
 }
 
 // socket.broadcast.emit('mouse',data); //Only to other clients
